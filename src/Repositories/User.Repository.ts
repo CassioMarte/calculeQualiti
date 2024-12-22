d45b3f17934cdc39df3275ddc97aede4f58be231
CassioMarte <135394169+CassioMarte@users.noreply.github.com>
2024-12-22 08:10:34 +0000

    Repositories e injeção de dependencia

---
+++ src/Repositories/User.Repository.ts
@@ -0,0 +1,26 @@
+import { Repository } from "typeorm";
+import { Users } from "../database/entities/User.entities";
+import { AppDataSource } from "../database/datasource";
+
+
+export class UserRepository{
+    //só posso usar metodos criados aqui
+    //private repository: Repository<Users>;
+    repository: Repository<Users>;    //acesso a tudo que Repository tem
+ 
+    constructor(){
+        this.repository = AppDataSource.getRepository(Users);
+    }
+
+    async findByEmail(email:string){
+        const emailExists = await this.repository.findOne({where:{email}})
+
+        return emailExists
+    }
+}
+
+/**
+ * criamos repositories personalizados e especializados por segurança
+ * e para que possamos criar metodos e funcões propria
+ */
+
--- src/controllers/User.Controller.ts
+++ src/controllers/User.Controller.ts
@@ -1,10 +1,159 @@
-import {Request, Response} from 'express'
+import { Request, Response } from 'express'
+// import { AppDataSource } from '../database/datasource'
+import { Users } from '../database/entities/User.entities'
+import { AppError } from '../error/AppError'
 
-export class UserController{
-     async createUser(req: Request, res:Response):Promise<any>{
-       const body = req.body
+//repositorio externo e personalizado
+import { UserRepository } from '../Repositories/User.Repository'
 
-       return res.send('Usuário criado com sucesso!');
-     }
+/**
+ * forma com injeção de dependencia 
+ * 
+ */
+
+// Interface para o corpo da requisição
+export interface CreateUserRequest extends Request {
+  body: {
+    name: string;
+    email: string;
+    phone: string;
+  };
 }
+
+export class UserController {
+  private userRepository: UserRepository;
+
+  // Injeção de dependência via construtor
+  constructor(userRepository: UserRepository) {
+    this.userRepository = userRepository;
+  }
+
+  async createUser(req: CreateUserRequest, res: Response): Promise<Response> {
+    const { name, email, phone } = req.body;
+
+    try {
+      const userExists = await this.userRepository.findByEmail(email);
+      if (userExists) {
+        throw new AppError('User already exists');
+      }
+
+      const dataUser = this.userRepository.repository.create({ name, email, phone });
+      const newUser = await this.userRepository.repository.save(dataUser);
+
+      return res.status(201).json({
+        message: "Usuario criado com sucesso",
+        data: newUser
+      });
+
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+}
+
+
+/**
+ * terceira forma com constant
+ */
+
+// export interface CreateUserRequest extends Request {
+//   body: {
+//     name: string;
+//     email: string;
+//     phone: string;
+//   };
+// }
+
+
+// export class UserController{
+//   async createUser(req:CreateUserRequest, res:Response):Promise<Response>{
+//     const userRepository = new UserRepository()
+//     const {name, email, phone} = req.body
+
+//     try {
+//       const userExists = await userRepository.findByEmail(email) 
+     
+//       if(userExists){
+//         throw new AppError('User already exists')
+//       }
+
+//     const dataUser = userRepository.repository.create({
+//       name, email, phone
+//     })
+
+//     const newUser = await userRepository.repository.save(dataUser)
+
+//     return res.status(201).json({
+//               message: "Usuario criado com sucesso",
+//               data: newUser
+//             })
+      
+//     } catch (error) {
+//       console.error(error)
+//       throw error
+//     }
+//   }
+// }
+
+
+/**
+ * Segunda forma melhor que a primeira mas eu estou usando .getRepository(Users) dentro 
+ * da controller o que não é o ideal; por isso criamos Repositories
+ */
+
+// export class UserController {
+
+//   async createUser(req: Request, res: Response): Promise<Response> {
+//     const userRepository = AppDataSource.getRepository(Users)
+//     const { name, email, phone } = req.body
+
+//     try {
+//       const userExistits = await userRepository.findOne({where:{email}})
+     
+//       if(userExistits){
+//        throw new AppError("User already exists")
+//       }
+      
+//       const dataUser = userRepository.create({
+//         name, email, phone
+//       })
+
+//       const newUser = userRepository.save(dataUser)
+
+//       return res.status(201).json({
+//         message: "Usuario criado com sucesso",
+//         data: newUser
+//       })
+//     } catch (error) {
+//       console.error(error)
+//       throw error
+//     }
+//   }
+// }
+
+/**
+ * Primeira forma a mais simples
+ */
+
+// export class UserController {
+//   async createUser(req: Request, res: Response): Promise<Response> {
+//     const { name, email, phone } = req.body
+
+//     const userRepository = AppDataSource.getRepository(Users)
+
+//     const dataUser = userRepository.create({
+//       name,
+//       email,
+//       phone
+//     })
+
+//    const newUser = await userRepository.save(dataUser)
+
+//    return res.status(201).json({
+//     message: "Usuario criado com sucesso",
+//     data: newUser
+//    })
+//   }
+// }
 
