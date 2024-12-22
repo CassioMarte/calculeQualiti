f061702782a1bb7bb0b070b498628a820e462e12
CassioMarte <135394169+CassioMarte@users.noreply.github.com>
2024-12-22 04:38:35 +0000

    controller

---
+++ src/controllers/User.Controller.ts
@@ -0,0 +1,11 @@
+import {Request, Response} from 'express'
+
+ class UserController{
+     async createUser(req: Request, res:Response){
+       const body = req.
+
+
+     }
+}
+
+export {UserController}
