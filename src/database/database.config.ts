8fcc6e9eec75eb50dded7d4f06713e6e735aff80
CassioMarte <135394169+CassioMarte@users.noreply.github.com>
2024-12-30 18:12:51 -0300

    Update database.config.ts

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -37,6 +37,40 @@ export const getDatabaseConfig = (env: 'production' | 'test'):DataSourceOptions=
     return configurations[env];
 }
 
+
+// . synchronize
+// Descrição: Quando synchronize está definido como true, o TypeORM sincroniza automaticamente o esquema do banco de dados com base nas suas entidades. Ele cria, atualiza ou remove tabelas e colunas no banco de dados para refletir as definições das entidades do TypeORM.
+// Uso:
+// Ideal para ambientes de desenvolvimento, pois facilita criar e ajustar rapidamente as tabelas à medida que você modifica suas entidades.
+// Não recomendado para produção, pois pode causar perda de dados se algo for alterado inadvertidamente.
+// Exemplo:
+// Se você adicionar uma nova propriedade em uma entidade, o TypeORM automaticamente adicionará uma nova coluna correspondente no banco.
+// 2. migrationsRun
+// Descrição: Quando migrationsRun está definido como true, o TypeORM executa automaticamente todas as migrações pendentes sempre que você inicializa a conexão com o banco de dados.
+// Uso:
+// Conveniente para manter o banco de dados atualizado em ambientes de desenvolvimento ou teste sem precisar rodar manualmente os comandos para executar migrações.
+// Cuidados:
+// No ambiente de produção, isso pode ser perigoso sem planejamento, pois qualquer erro em uma migração pode comprometer o banco de dados.
+// 3. logging
+// Descrição: Controla se e como o TypeORM registra logs de atividades realizadas no banco de dados, como consultas SQL, erros ou avisos.
+// Opções:
+// true: Registra todas as consultas SQL e atividades (recomendado para depuração).
+// false: Nenhum log é gerado.
+// Tipos específicos:
+// Você pode especificar os tipos de log, como "query", "error", "schema", "warn", "info", "log".
+// Exemplo:
+// javascript
+// Copiar código
+// logging: ['query', 'error'] // Apenas consultas e erros são registrados.
+// Uso:
+// Útil para depuração e análise de desempenho no desenvolvimento ou para monitorar falhas e comportamento do banco de dados.
+
+
+
+
+
+
+
 // import { DataSourceOptions } from 'typeorm';
 // import dotenv from 'dotenv';
 
